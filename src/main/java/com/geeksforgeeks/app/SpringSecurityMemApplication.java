df0040790f4da59ef0b4fe68f575ff7c6d76dcb8
Dharmendra Kumar Awasthi <awadha01@CSGICORP.COM>
2020-09-27 20:28:50 +0530

    Controller init

---
+++ src/main/java/com/geeksforgeeks/app/SpringSecurityMemApplication.java
@@ -0,0 +1,13 @@
+package com.geeksforgeeks.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMemApplication.class, args);
+	}
+
+}
